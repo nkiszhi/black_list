3e22084fe1223b4b963736541c3589155c8f1cae
DengCongyi <38749763+dengcongyi0701@users.noreply.github.com>
2020-10-21 16:07:31 +0800

    基于人工特征工程的APT域名检测

---
+++ APT/detection.py
@@ -0,0 +1,286 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2020/9/14 9:36
+
+@author : dengcongyi0701@163.com
+
+Description:
+
+"""
+import pandas as pd
+import numpy as np
+import pickle
+from sklearn.preprocessing import StandardScaler
+from xgboost import XGBClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import roc_curve
+from sklearn.metrics import plot_roc_curve
+import matplotlib.pyplot as plt
+
+from feature_extraction import wash_tld, phishing_get_feature
+
+class RF_classifier:
+
+    def __init__(self):
+        self.RF_clf = RandomForestClassifier(n_estimators=100, criterion='entropy',
+                                             random_state=23, n_jobs=-1, max_features=20)
+        self.standardScaler = StandardScaler()
+        self.train_score = None
+        self.isload_ = False
+
+    def train(self, model_folder, train_feature_add):
+        """
+        BRF算法训练数据
+        :param train_feature_add: 训练数据路径
+        :param model_add:  模型存储路径
+        :return:
+        """
+        train_df = pd.read_csv(train_feature_add, index_col=['domain_name'])
+        train_df = train_df.fillna('0.0')
+        x_train = train_df.drop(['label'], axis=1).values
+        y_train = train_df['label'].values
+        print("_______RF Training_______")
+        self.RF_clf.fit(x_train, y_train)
+        mal_scores = np.array(self.RF_clf.predict_proba(x_train))[:, 1]
+        mal_scores = sorted(mal_scores)
+        np.save(r"{}\RF_train_scores.npy".format(model_folder), mal_scores)
+        pickle.dump(self.RF_clf, open("{}\RF_model.pkl".format(model_folder), 'wb'))
+
+    def load(self, model_folder):
+        """
+        将模型文件和归一化尺度读取到内存中
+        :param model_add: 模型存储路径
+        :param standard_scaler_add: 归一化scaler存储路径
+        :return:
+        """
+        self.RF_clf = pickle.load(open("{}\RF_model.pkl".format(model_folder), 'rb'))
+        self.standardScaler = pickle.load(open("{}\standardscalar.pkl".format(model_folder), 'rb'))
+        self.train_score = np.load(r"{}\RF_train_scores.npy".format(model_folder))
+        self.isload_ = True
+
+    def predict(self, model_folder, test_feature_add):
+        """
+        测试集进行测试，计算准确率等
+        :param test_feature_add: 测试数据路径
+        :return:
+        """
+        self.load(model_folder)
+        test_df = pd.read_csv(test_feature_add, index_col=['domain_name'])
+        test_df = test_df.fillna('0.0')
+        x_test = test_df.drop(['label'], axis=1).values
+        y_test = test_df['label'].values
+        print("_______RF Predicting_______")
+        y_predict = self.RF_clf.predict(x_test)
+        print("RF accuracy: ", self.RF_clf.score(x_test, y_test))
+        print("RF precision: ", precision_score(y_test, y_predict, average='macro'))
+        print("RF recall: ", recall_score(y_test, y_predict, average='macro'))
+        print("RF F1: ", f1_score(y_test, y_predict, average='macro'))
+        print("RF TPR, FPR, thresholds: ", roc_curve(y_test, y_predict, pos_label=1))
+
+        plot_roc_curve(self.RF_clf, x_test, y_test)
+        plt.show()
+
+    def predict_singleDN(self, model_folder, dname):
+        """
+        对单个域名进行检测，输出检测结果及恶意概率
+        :param dname: 域名
+        :return:
+        """
+        if not self.isload_:
+            self.load(model_folder)
+        dname = dname.replace("www.", "")
+        dname = wash_tld(dname)
+        if dname == "":
+            label = 0
+            prob = 0.0000
+            p_value = 1.0000
+            print("label:", label)
+            print("mal_prob:", prob)
+            print("p_value:", p_value)
+            return label, prob, p_value
+        else:
+            feature = self.standardScaler.transform(pd.DataFrame([get_feature(dname)]))
+            label = self.RF_clf.predict(feature)
+            prob = self.RF_clf.predict_proba(feature)
+            p_value = cal_pValue(self.train_score, prob[0][1], label[0])
+            print("label:", label[0])
+            print("mal_prob:", prob[0][1])
+            print("p_value:", p_value)
+            return label[0], prob[0][1], p_value
+
+
+class XGBoost_classifier:
+
+    def __init__(self):
+        self.XGBoost_clf = XGBClassifier(max_depth=5, learning_rate=0.1, n_estimator=100, silent=True,
+                                         objective='binary:logistic')
+        self.standardScaler = StandardScaler()
+        self.train_score = None
+        self.isload_ = False
+
+    def train(self, model_folder, train_feature_add):
+        """
+        XGBoost算法训练数据
+        :param train_feature_add: 训练数据路径
+        :param model_add:  模型存储路径
+        :return:
+        """
+        train_df = pd.read_csv(train_feature_add, index_col=['domain_name'])
+        train_df = train_df.fillna('0.0')
+        x_train = train_df.drop(['label'], axis=1).values
+        y_train = train_df['label'].values
+        print("_______XGBoost Training_______")
+        self.XGBoost_clf.fit(x_train, y_train)
+        mal_scores = np.array(self.XGBoost_clf.predict_proba(x_train))[:, 1]
+        mal_scores = sorted(mal_scores)
+        np.save(r"{}\XGBoost_train_scores.npy".format(model_folder), mal_scores)
+        pickle.dump(self.XGBoost_clf, open("{}\XGBoost_model.pkl".format(model_folder), 'wb'))
+
+    def load(self, model_folder):
+        """
+        将模型文件和归一化尺度读取到内存中
+        :param model_add: 模型存储路径
+        :param standard_scaler_add: 归一化scaler存储路径
+        :return:
+        """
+        self.XGBoost_clf = pickle.load(open("{}\XGBoost_model.pkl".format(model_folder), 'rb'))
+        self.standardScaler = pickle.load(open("{}\standardscalar.pkl".format(model_folder), 'rb'))
+        self.train_score = np.load(r"{}\XGBoost_train_scores.npy".format(model_folder))
+        self.isload_ = True
+
+    def predict(self, model_folder, test_feature_add):
+        """
+        测试集进行测试，计算准确率等
+        :param test_feature_add: 测试数据路径
+        :return:
+        """
+        self.load(model_folder)
+        test_df = pd.read_csv(test_feature_add, index_col=['domain_name'])
+        test_df = test_df.fillna('0.0')
+        x_test = test_df.drop(['label'], axis=1).values
+        y_test = test_df['label'].values
+        print("_______XGBoost Predicting_______")
+        y_predict = self.XGBoost_clf.predict(x_test)
+        print("XGBoost accuracy: ", self.XGBoost_clf.score(x_test, y_test))
+        print("XGBoost precision: ", precision_score(y_test, y_predict, average='macro'))
+        print("XGBoost recall: ", recall_score(y_test, y_predict, average='macro'))
+        print("XGBoost F1: ", f1_score(y_test, y_predict, average='macro'))
+        print("XGBoost TPR, FPR, thresholds: ", roc_curve(y_test, y_predict, pos_label=1))
+
+        plot_roc_curve(self.XGBoost_clf, x_test, y_test)
+        plt.show()
+
+    def predict_singleDN(self, model_folder, dname):
+        """
+        对单个域名进行检测，输出检测结果及恶意概率
+        :param dname: 域名
+        :return:
+        """
+        if not self.isload_:
+            self.load(model_folder)
+        dname = dname.replace("www.", "")
+        dname = wash_tld(dname)
+        if dname == "":
+            label = 0
+            prob = 0.0000
+            p_value = 1.0000
+            print("label:", label)
+            print("mal_prob:", prob)
+            print("p_value:", p_value)
+            return label, prob, p_value
+        else:
+            feature = self.standardScaler.transform(pd.DataFrame([phishing_get_feature(dname)]))
+            label = self.XGBoost_clf.predict(feature)
+            prob = self.XGBoost_clf.predict_proba(feature)
+            p_value = cal_pValue(self.train_score, prob[0][1], label[0])
+            print("label:", label[0])
+            print("mal_prob:", prob[0][1])
+            print("p_value:", p_value)
+            return label[0], prob[0][1], p_value
+
+
+def cal_pValue(score_list, key, label):
+    """
+    计算p_value
+    :param score_list: 训练集得分列表
+    :param key: 测试样本得分
+    :param label: 测试样本标签
+    :return: p_value, 保留四位小数
+    """
+    count = 0
+    if label == 0:
+        temp = sorted(score_list, reverse=1)
+        score_list = [i for i in temp if i <= 0.5]
+        left = 0
+        right = len(score_list) - 1
+        while left <= right:
+            middle = (left+right)//2
+            if key < score_list[middle]:
+                left = middle + 1
+            elif key > score_list[middle]:
+                right = middle - 1
+            else:
+                count = middle + 1
+                break
+        count = left
+    elif label == 1:
+        temp = sorted(score_list, reverse=0)
+        score_list = [i for i in temp if i > 0.5]
+        left = 0
+        right = len(score_list) - 1
+        while left <= right:
+            middle = (left+right)//2
+            if key > score_list[middle]:
+                left = middle + 1
+            elif key < score_list[middle]:
+                right = middle - 1
+            else:
+                count = middle + 1
+                break
+        count = left
+    p_value = count/len(score_list)
+    return round(p_value, 4)
+
+
+if __name__ == "__main__":
+    train_add = r"M:\APT\features\train_features.csv"
+    test_add = r"M:\APT\features\test_features.csv"
+    RF_model_add = r"M:\APT\model\RF_model.pkl"
+    XGBoost_model_add = r"M:\APT\model\XGBoost_model.pkl"
+    standard_scaler_add = r"M:\APT\model\standardscalar.pkl"
+
+    phishing_train_add = r"M:\APT\features\phishing_train_features.csv"
+    phishing_test_add = r"M:\APT\features\phishing_test_features.csv"
+    phishing_model_folder = r"M:\APT\model\phishing"
+
+    # RF_clf = RF_classifier()
+    # RF_clf.train(phishing_model_folder, phishing_train_add)
+    # RF_clf.predict(phishing_model_folder, phishing_test_add)
+    # RF_clf.predict_singleDN(phishing_model_folder, "nkamg.dsalkswjgoijdslk.com")
+
+
+    XGBoost_clf = XGBoost_classifier()
+    # XGBoost_clf.train(phishing_model_folder, phishing_train_add)
+    # XGBoost_clf.predict(phishing_model_folder, phishing_test_add)
+    # XGBoost_clf.predict_singleDN(phishing_model_folder, "urldetec.gcowsec.com")
+
+    XGBoost_clf = XGBoost_classifier()
+    mal = 0
+    benigh = 0
+    with open("1.txt", "r") as f:
+        for line in f.readlines():
+            name = line.strip()
+            print("-----------------------------")
+            print(name)
+            label, mal_prob, p_value = XGBoost_clf.predict_singleDN(phishing_model_folder, name)
+            if label:
+                mal += 1
+            else:
+                benigh += 1
+    print("-----------------------------")
+    print("1:{}, 0:{}".format(mal, benigh))
+    print("accu:", mal/(mal+benigh))
+
---
+++ APT/feature_extraction.py
@@ -0,0 +1,310 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2020/9/13 18:11
+
+@author : dengcongyi0701@163.com
+
+Description:
+
+"""
+import re
+import pickle
+import math
+import wordfreq
+import operator
+import numpy as np
+import pandas as pd
+from collections import Counter, defaultdict
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+hmm_add = r".\static\hmm_matrix.csv"
+gib_add = r".\static\gib_model.pki"
+gramfile_add = r".\static\n_gram_rank_freq.txt"
+private_tld_file = r".\static\private_tld.txt"
+tld_add = r".\static\tld.txt"
+tld_rank_add = r".\static\tld_rank.txt"
+white_file_add = r"M:\APT\data\sample\sample_white.csv"
+black_file_add = r"M:\APT\data\sample\sample_black.csv"
+phishing_file_add = r"M:\APT\data\sample\phishing.csv"
+hmm_prob_threshold = -120
+tld_list = list()
+with open(tld_add, 'r', encoding='utf8') as f:
+    for i in f.readlines():
+        tld_list.append(i.strip().strip('.'))
+accepted_chars = 'abcdefghijklmnopqrstuvwxyz '
+pos = dict([(char, idx) for idx, char in enumerate(accepted_chars)])
+abuse_tld = ['email', 'fit', 'tk', 'fail', 'run', 'rest', 'ml', 'cn', 'viajes', 'cf', 'recipes', 'gq', 'ga']
+
+feature_dir = r"M:\APT\features"
+model_dir = r"M:\APT\model"
+
+def wash_tld(dn):
+    """
+    将域名字符串中顶级域名去掉，剩余部分拼接成完整字符串
+    :param dn: 原始域名
+    :return: 拼接字符串
+    """
+    dn_list = dn.split('.')
+    dn_list = list(set(dn_list).difference(set(tld_list)))
+    namestring = "".join(dn_list)
+    return namestring
+
+def cal_rep_cart(ns):
+    """
+    计算字符串中重复出现的字符个数
+    :param SLD: 字符串
+    :return: 重复字符个数
+    """
+    count = Counter(i for i in ns).most_common()
+    sum_n = 0
+    for letter, cnt in count:
+        if cnt > 1:
+            sum_n += 1
+    return sum_n
+
+def cal_ent_gni_cer(SLD):
+    """
+    计算香农熵, Gini值, 字符错误的分类
+    :param url:
+    :return:
+    """
+    f_len = float(len(SLD))
+    count = Counter(i for i in SLD).most_common()  # unigram frequency
+    ent = -sum(float(j / f_len) * (math.log(float(j / f_len), 2)) for i, j in count)  # shannon entropy
+    gni = 1 - sum(float(j / f_len) * float(j / f_len) for i, j in count)
+    cer = 1 - max(float(j/ f_len) for i, j in count)
+    return ent, gni, cer
+
+
+def cal_gram_med(SLD, n):
+    """
+    计算字符串n元频率中位数
+    :param SLD: 字符串
+    :param n: n
+    :return:
+    """
+    grams = [SLD[i:i + n] for i in range(len(SLD) - n+1)]
+    fre = list()
+    for s in grams:
+        fre.append(wordfreq.zipf_frequency(s, 'en'))
+    return np.median(fre)
+
+
+def cal_hmm_prob(url):
+    """
+    计算成文概率, 结果越小越异常
+    :param url:
+    :return: 概率
+    """
+    hmm_dic = defaultdict(lambda: defaultdict(float))
+    with open(hmm_add, 'r') as f:
+        for line in f.readlines():
+            key1, key2, value = line.rstrip().split('\t')  # key1 can be '' so rstrip() only
+            value = float(value)
+            hmm_dic[key1][key2] = value
+    url = '^' + url.strip('.') + '$'
+    gram2 = [url[i:i+2] for i in range(len(url)-1)]
+    prob = hmm_dic[''][gram2[0]]
+
+    for i in range(len(gram2)-1):
+        prob *= hmm_dic[gram2[i]][gram2[i+1]]
+    if prob < math.e ** hmm_prob_threshold:
+        prob = -999
+    return prob
+
+
+def cal_gib(SLD):
+    """
+    计算gib标签
+    :param SLD:
+    :return: 1: 正常 0: 异常
+    """
+    gib_model = pickle.load(open(gib_add, 'rb'))
+    mat = gib_model['mat']
+    threshold = gib_model['thresh']
+
+    log_prob = 0.0
+    transition_ct = 0
+    SLD = re.sub("[^a-z]", "", SLD)
+    gram2 = [SLD[i:i + 2] for i in range(len(SLD) - 1)]
+    for a, b in gram2:
+        log_prob += mat[pos[a]][pos[b]]
+        transition_ct += 1
+    # The exponentiation translates from log probs to probs.
+    prob = math.exp(log_prob / (transition_ct or 1))
+    return int(prob > threshold)
+
+
+def load_gramdict():
+    """
+    加载n元排序字典
+    :return: 字典
+    """
+    rank_dict = dict()
+    with open(gramfile_add, 'r') as f:
+        for line in f:
+            cat, gram, freq, rank = line.strip().split(',')
+            rank_dict[gram] = int(rank)
+    return rank_dict
+
+
+##############################钓鱼网站###############################
+def phishing_get_feature(dn):
+    """
+    钓鱼网站特征提取
+    :param url: 域名
+    :return: 25维特征
+    """
+    paras = dn.split('.')
+    ns = re.sub(r"\.", "", dn)
+    namestring = re.sub(r"\.|_|-", "", dn)
+    tld_rank_dict = load_tlddict()
+
+    # 1.段数
+    para_sum = len(paras)
+    # 2.包含TLD数量
+    tlds = list(set(paras) & set(tld_list))
+    tld_sum = len(tlds)
+    # 3.包含被滥用TLD数量
+    abuse_sum = len(set(paras) & set(abuse_tld))
+    # 4.包含恶意TLD排序均值
+    tldrank = [tld_rank_dict[t] for t in tlds]
+    if tldrank:
+        tldrank_avg = np.mean(tldrank)
+    else:
+        tldrank_avg = -1
+    # 5. 字符串长度
+    string_len = len(namestring)
+    # 6. 字符串不重复字符数
+    uni_string = len(set(namestring))
+    # 7. 是否以数字开头
+    flag_dig = 0
+    if re.match("[0-9]", namestring) != None:
+        flag_dig = 1
+    # 8. 特殊符号在字符串中占比
+    sym = len(re.findall(r"\.|_|-", dn))/string_len
+    # 9. 数字在字符串中占比
+    dig = len(re.findall(r"[0-9]", namestring))/string_len
+    # 10. 元音字母在字符串中占比
+    vow = len(re.findall(r"a|e|i|o|u", namestring))/string_len
+    # 11. 辅音字母在字符串中占比
+    con = len(re.findall(r"b|c|d|f|g|h|j|k|l|m|n|p|q|r|s|t|v|w|x|y|z", namestring))/string_len
+    # 12. 重复字符在字符串不重复字符中占比
+    rep_char_ratio = cal_rep_cart(namestring)/uni_string
+    # 13. 域名中连续辅音占比
+    con_list = re.findall(r"[b|c|d|f|g|h|j|k|l|m|n|p|q|r|s|t|v|w|x|y|z]{2,}", namestring)
+    con_len = [len(con) for con in con_list]
+    cons_con_ratio = sum(con_len)/string_len
+    # 14. 域名中连续数字占比
+    dig_list = re.findall(r"[0-9]{2,}", namestring)
+    dig_len = [len(dig) for dig in dig_list]
+    cons_dig_ratio = sum(dig_len)/string_len
+    # 15. 字符串中由'-'分割的令牌数
+    tokens_sld = len(ns.split('-'))
+    # 16. 字符串中数字总数
+    digits_sld = len(re.findall(r"[0-9]", namestring))
+    # # 14. SLD中字符的归一化熵值
+    # # 15. SLD的Gini值
+    # # 16. SLD中字符分类的错误
+    # ent, gni, cer = cal_ent_gni_cer(namestring)
+
+
+    feature = [para_sum, tld_sum, abuse_sum, tldrank_avg, string_len, uni_string, flag_dig, sym, dig, vow, con,
+               rep_char_ratio, cons_con_ratio, cons_dig_ratio, tokens_sld, digits_sld]
+    return feature
+
+def phishing_feature_extraction(df):
+    """
+    特征提取, 归一化
+    :param df:
+    :return:
+    """
+    col = ["domain_name", "label", "para_sum", "tld_sum", "abuse_sum", "tldrank_avg", "string_len", "uni_string",
+           "flag_dig", "sym", "dig", "vow", "con", "rep_char_ratio", "cons_con_ratio", "cons_dig_ratio", "tokens_sld",
+           "digits_sld"]
+    fea_list = list()
+    for ind in df.index:
+        fea = df.loc[ind].tolist()
+        if ind % 1000 == 0:
+            print("{}...".format(ind))
+        fea.extend(phishing_get_feature(df.at[ind, 0]))
+        fea_list.append(fea)
+    fea_df = pd.DataFrame(fea_list, columns=col)
+
+    return fea_df
+
+
+def phishing_dataset_generation():
+    """
+    数据集划分,
+    :return:
+    """
+
+    bk_df = pd.read_csv(phishing_file_add, header=None)
+    wh_df = pd.read_csv(white_file_add, header=None)
+
+    df = bk_df.append(wh_df, ignore_index=True)
+    df_train, df_test = train_test_split(df, test_size=0.2, stratify=df[1], random_state=23)
+    df_train = df_train.reset_index(drop=True)
+    df_test = df_test.reset_index(drop=True)
+
+    print("__________Generating Train Set__________")
+    train_feature = phishing_feature_extraction(df_train)
+    # train_feature.to_csv(r"{}\raw_train_features.csv".format(feature_dir), index=None)
+    train_feature = train_feature.set_index(['domain_name', 'label'])
+    standardScaler = StandardScaler()
+    standardScaler.fit(train_feature.values)
+
+    print("__________Generating Test Set__________")
+    test_feature = phishing_feature_extraction(df_test)
+    # test_feature.to_csv(r"{}\raw_test_features.csv".format(feature_dir), index=None)
+    test_feature = test_feature.set_index(['domain_name', 'label'])
+
+    train_feature = pd.DataFrame(standardScaler.transform(train_feature), index=train_feature.index,
+                                 columns=train_feature.columns)
+    train_feature = train_feature.reset_index()
+    train_feature.to_csv(r"{}\phishing_train_features.csv".format(feature_dir), index=None)
+    test_feature = pd.DataFrame(standardScaler.transform(test_feature), index=test_feature.index,
+                                columns=test_feature.columns)
+    test_feature = test_feature.reset_index()
+    test_feature.to_csv(r"{}\phishing_test_features.csv".format(feature_dir), index=None)
+    pickle.dump(standardScaler, open(r"{}\phishing\standardscalar.pkl".format(model_dir), 'wb'))
+    return
+
+def create_TLD_rank():
+    """
+    根据钓鱼网站黑名单统计顶级域名危险程度排名
+    :return:
+    """
+    tld_sum = dict()
+    for tld in tld_list:
+        tld_sum[tld] = 0
+    with open(phishing_file_add, 'r', encoding='utf8') as f:
+        for line in f.readlines():
+            paras = line.split(',')[0].split('.')
+            intersec = list(set(paras) & set(tld_list))
+            for t in intersec:
+                tld_sum[t] += 1
+    tld_sum = dict(sorted(tld_sum.items(), key=operator.itemgetter(1), reverse=1))
+    df = pd.DataFrame.from_dict(tld_sum, orient='index')
+    rank = [i+1 for i in range(len(tld_sum))]
+    df[1] = rank
+    df.to_csv(tld_rank_add, header=False, encoding='utf8')
+
+def load_tlddict():
+    rank_dict = dict()
+    with open(tld_rank_add, 'r', encoding='utf8') as f:
+        for line in f:
+            tld, freq, rank = line.strip().split(',')
+            rank_dict[tld] = int(rank)
+    return rank_dict
+
+if __name__ == "__main__":
+
+    # dataset_generation()
+    phishing_dataset_generation()
+
+
+
+
